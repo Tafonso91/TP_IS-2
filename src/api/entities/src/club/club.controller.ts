e17135c854889dfb222878fc9c9f5d0866548ac5
Tafonso91 <t.veloso.afonso@ipvc.pt>
2024-01-04 16:22:53 +0000

    commit do post do api entititties

--- src/api/entities/src/club/club.controller.ts
+++ src/api/entities/src/club/club.controller.ts
@@ -1,15 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, HttpStatus, HttpException, Param } from '@nestjs/common';
 import { ClubService } from './club.service';
 
 @Controller('club')
 export class ClubController {
-constructor(private readonly clubService: ClubService) {}
+    constructor(private readonly clubService: ClubService) {}
 
-@Get()
+    @Get()
     async findAll() {
         return this.clubService.findAll();
     }
+
+    @Post(':clubName')
+    async createClub(@Param('clubName') clubName: string) {
+        try {
+            const clubData = {
+                club_name: clubName,
+   
+            };
+            return await this.clubService.createClub(clubData);
+        } catch (error) {
+            throw new HttpException('Failed to create club', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
 }
+
 
 
 
--- src/api/entities/src/club/club.service.ts
+++ src/api/entities/src/club/club.service.ts
@@ -8,6 +8,14 @@ export class ClubService {
     async findAll(): Promise<any[]> {
         return this.prisma.club.findMany();
     }
+    async createClub(clubData: { club_name: string }): Promise<any> {
+        return this.prisma.club.create({
+            data: {
+                club_name: clubData.club_name,
+                
+            },
+        });
+    }
 }
 
 
