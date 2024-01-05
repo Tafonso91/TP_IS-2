9e520800db01e58aac3c19818c2011ba5eaf7ed8
Tafonso91 <t.veloso.afonso@ipvc.pt>
2024-01-05 19:28:18 +0000

    frontend ent quase falta o CORS

--- src/frontend/ent/app/countries/page.js
+++ src/frontend/ent/app/countries/page.js
@@ -1,3 +1,32 @@
+"use client";
+import React, { useEffect, useState } from 'react';
+import crudAPI from '../crud/crudAPI'; 
+
 export default function CountriesPage() {
-    return <main><b>Countries Page</b>: Work in progress</main>;
+  const [clubs, setClubs] = useState([]);
+
+  useEffect(() => {
+    const fetchClubs = async () => {
+      try {
+        const response = await crudAPI().GET('/country'); // Chamada para listar os clubes
+        setClubs(response.data); // Define os clubes no estado local
+      } catch (error) {
+        console.error('Erro ao buscar clubes:', error);
+      }
+    };
+
+    fetchClubs();
+  }, []);
+
+  return (
+    <main>
+      <b>Countries Page</b>: 
+      <ul>
+        {clubs.map((club) => (
+          <li key={club.id}>{club.club_name}</li> 
+        ))}
+      </ul>
+    </main>
+  );
 }
+
