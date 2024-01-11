1f138c24c5728fd4529b8cac8e33be20ef88d6d7
Tafonso91 <t.veloso.afonso@ipvc.pt>
2024-01-11 18:46:41 +0000

    tentando

--- src/migrator1/main.go
+++ src/migrator1/main.go
@@ -1,11 +1,12 @@
 package main
-
 import (
 	"database/sql"
 	"encoding/xml"
+	"encoding/json"
 	"fmt"
 	"log"
 	"strings"
+	"time"
 
 	"github.com/antchfx/xmlquery"
 	_ "github.com/lib/pq"
@@ -18,7 +19,9 @@ type Country struct {
 	Id      string   `xml:"Id,attr"`
 	Name    string   `xml:"Name,attr"`
 }
-const apiCountriesCreate = "http://localhost:8080/country"
+
+const apiCountriesCreate = "http://localhost:8080/country" 
+
 func main() {
 	connectionString := "postgres://is:is@db-xml/is?sslmode=disable"
 
@@ -67,61 +70,57 @@ func main() {
 
 		fmt.Printf("Nome do arquivo do XML recebido: %s\n", fileName)
 
-		// Consulta para obter o conteúdo XML do banco de dados com base no fileName
 		xmlQuery := "SELECT xml FROM public.imported_documents WHERE file_name = $1"
 
 		var xmlData string
-
-		// Executar a consulta para obter o conteúdo XML do banco de dados
 		err := db.QueryRow(xmlQuery, fileName).Scan(&xmlData)
 		if err != nil {
 			log.Fatalf("Erro ao buscar XML do banco de dados: %s", err)
 		}
 
-		// Parse do documento XML usando xmlquery
 		doc, err := xmlquery.Parse(strings.NewReader(xmlData))
 		if err != nil {
 			log.Fatalf("Erro ao analisar o XML: %s", err)
 		}
 
-		nodes := xmlquery.Find(doc, "//Countries/Country")
+		nodes := xmlquery.Find(doc, "//Countries/Country/@Name")
 		for _, country := range nodes {
 			var name string
-	
+
 			for _, attr := range country.Attr {
 				if attr.Name.Local == "Name" {
 					name = attr.Value
 					break
 				}
 			}
-	
+
 			payload := map[string]string{"name": name}
 			jsonData, err := json.Marshal(payload)
 			if err != nil {
-				return err
+				log.Fatal(err)
 			}
-	
+
 			client := resty.New()
 			resp, err := client.R().
 				SetHeader("Content-Type", "application/json").
 				SetBody(jsonData).
 				Post(apiCountriesCreate)
-	
+
 			if err != nil {
 				log.Println("Erro ao enviar solicitação:", err)
-				return err
+				log.Fatal(err)
 			}
-	
+
 			log.Printf("Resposta da API: Status %d\n", resp.StatusCode())
-	
+
 			if resp.StatusCode() != 201 {
-				return fmt.Errorf("Falha ao chamar a API. Status: %d", resp.StatusCode())
+				log.Fatalf("Falha ao chamar a API. Status: %d", resp.StatusCode())
 			}
-	
+
 			log.Printf("Corpo da resposta: %s\n", resp.Body())
-	
+
 			time.Sleep(1 * time.Millisecond)
 		}
-	
-		return nil
+		
 	}
+}
